7e06f13ab0a53a124a1776e7c3e30f56843254d8
Edwin Hui <edwinhuish@gmail.com>
2022-05-10 14:49:02 +0800

    更改代码结构并加入 loadmore

---
+++ index.js
@@ -0,0 +1,4 @@
+import LoadMore from './lib/load-more'
+import PullRefresh from './lib/pull-refresh'
+
+export { LoadMore, PullRefresh }
--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-const files = require.context('.', true, /\.vue$/)
-
-files.keys().forEach(filename => {
-  const module = files(filename).default
-
-  if (module.name) {
-    exports[module.name] = module
-  }
-})
